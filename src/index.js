986a947714cc8b3a1621f9940c2c9eb489dfd7f3
debahunter821-ux <debahunter821@gmail.com>
2026-05-29 16:33:59 +0100

    Add search engine

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInputElement.value.trim();
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
